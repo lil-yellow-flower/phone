9f2cf2b0c2173bccc3356b32f8c794e20fd78093
Mathias Ögärd <mathias-93@live.se>
2022-03-23 09:46:31 +0100

    normalize swedish phone numbers

--- main.go
+++ main.go
@@ -3,14 +3,27 @@ package main
 import (
 	// "bytes"
 	"regexp"
+	"strconv"
 )
 
 
 func normalize(phone string) string {
 	re := regexp.MustCompile("[^0-9]")
 
-	return re.ReplaceAllString(phone, "")
+	res := re.ReplaceAllString(phone, "")
+
+	i, err := strconv.Atoi(res[:2])
+	if err == nil {
+		if i == 46 {
+			len := len([]rune(res))
+			res = "0" + res[2:len]
+		}
+	}
+
+	return res
 }
+
+
 
 // func normalize(phone string) string {
 // 	var buf bytes.Buffer
